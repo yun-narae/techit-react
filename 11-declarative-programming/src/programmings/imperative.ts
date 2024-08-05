7c8b2fd06e95f85b2db2ca18bdaad34b3d3ec2e3
seulbinim <skfo0827@naver.com>
2024-08-05 10:16:09 +0900

    [학습] 명령형 프로그래밍 (HOW)
    - 문서 객체 접근
    - 접근한 객체에 이벤트 연결
    - 연결된 이벤트 핸들러(함수, 기능) 구현

--- 11-declarative-programming/src/programmings/imperative.ts
+++ 11-declarative-programming/src/programmings/imperative.ts
@@ -1,7 +1,31 @@
+
 // --------------------------------------------------------------------------
 // ✅ 명령형 프로그래밍
 // --------------------------------------------------------------------------
-// - [ ] 체크박스 인풋이 체크 상태이면, 버튼은 활성 상태입니다.
-// - [ ] 체크박스 인풋이 체크 상태가 아니면, 버튼은 비활성 상태입니다.
-// - [ ] 버튼 상태 변경에 따라 레이블은 "활성 상태", "비활성 상태"로 변경되어야 합니다.
+// - [x] 체크박스 인풋이 체크 상태이면, 버튼은 활성 상태입니다.
+// - [x] 체크박스 인풋이 체크 상태가 아니면, 버튼은 비활성 상태입니다.
+// - [x] 버튼 상태 변경에 따라 레이블은 "활성 상태", "비활성 상태"로 변경되어야 합니다.
 // --------------------------------------------------------------------------
+
+// 대상 찾기
+const container = document.getElementById('imperative-programming');
+const checkbox = container.querySelector('input[type="checkbox"]') as HTMLInputElement; // 타입 단언(Type Assertion)
+const button = container.querySelector('button');
+
+globalThis.checkbox = checkbox;
+
+// 찾은 대상에 이벤트 연결
+checkbox.addEventListener('change', handleChange);
+
+// 이벤트 핸들러(기능) 작성(구현)
+function handleChange(e) {
+  const { checked } = e.target;
+  if (checked) {
+    // button.setAttribute('disabled', 'false');
+    button.removeAttribute('disabled');
+    button.textContent = '활성 상태';
+  } else {
+    button.setAttribute('disabled', 'true');
+    button.textContent = '비활성 상태';
+  }
+}
